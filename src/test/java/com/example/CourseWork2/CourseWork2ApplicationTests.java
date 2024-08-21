7a8d595bd5fe7d600b69cf1fe8ff4207006bded4
Ilea <sorokin58473@gmail.com>
2024-08-21 20:46:37 +1000

    Начал

---
+++ src/test/java/com/example/CourseWork2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
